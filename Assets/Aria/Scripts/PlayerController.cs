78cd4437b645e98635a3ff87dbf43a318e0a8e52
ariaDalgleish <dalgleisharia07@gmail.com>
2024-07-28 18:28:40 +1200

    InProgress
    InputSystem player movement/controls. Haven't finished script/setup.

---
+++ Assets/Aria/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    public float playerSpeed = 2.0f;
+    public float jumpHeight = 1.0f;
+    public float gravityValue = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+
+    private Vector2 movementInput = Vector2.zero;
+    private bool jumped = false;
+
+    private void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        jumped = context.ReadValue<bool>();
+        jumped = context.action.triggered;
+    }
+
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector3 move = new Vector3(movementInput.x, 0, movementInput.y);
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        // Changes the height position of the player..
+        if (jumped && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Aria/Scripts/PlayerInput.cs
@@ -0,0 +1,136 @@
+//This script is used as an example of how to recieve inputs from a single device
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    /*
+     [SerializeField] int playerID; //This is the playerID that it will search for
+
+     //Below references are just to visualise inputs
+     [SerializeField] GameObject waitingGrp; //GameObject grp to show that its waiting for a player
+     [SerializeField] GameObject inputGrp; //GameObject grp to visualise inputs once a player has connected
+
+     [SerializeField] GameObject northButton;
+     [SerializeField] GameObject southButton;
+     [SerializeField] GameObject westButton;
+     [SerializeField] GameObject eastButton;
+     [SerializeField] Transform analogStick;
+     public Vector2 analogInput;
+
+     InputControls playerControls;
+
+     private void Start()
+     {
+         //At the start we will create a callback using the delegate within the InputManager script
+         //This will check for the PlayerJoin input to be pressed
+         InputManager.instance.onPlayerJoined += AssignInputs;
+     }
+
+     private void OnDisable()
+     {
+         //Cleaning up :)
+         InputManager.instance.onPlayerJoined -= AssignInputs;
+
+         if (playerControls != null)
+         {
+             playerControls.MasterActions.NorthButton.performed -= NorthButton_performed;
+             playerControls.MasterActions.NorthButton.canceled -= NorthButton_canceled;
+             playerControls.MasterActions.SouthButton.performed -= SouthButton_performed;
+             playerControls.MasterActions.SouthButton.canceled -= SouthButton_canceled;
+             playerControls.MasterActions.EastButton.performed -= EastButton_performed;
+             playerControls.MasterActions.EastButton.canceled -= EastButton_canceled;
+             playerControls.MasterActions.WestButton.performed -= WestButton_performed;
+             playerControls.MasterActions.WestButton.canceled -= WestButton_canceled;
+             playerControls.MasterActions.Movement.performed -= Movement_performed;
+             playerControls.MasterActions.Movement.canceled -= Movement_canceled;
+         }
+     }
+
+     //This function will be called if the Joined Button has been detected from any device
+     void AssignInputs(int ID)
+     {
+         //First we check if the player id matches the ID of the device that has been pressed
+         if (playerID == ID)
+         {
+             //If it matches, we can move the callback since the player has been found
+             InputManager.instance.onPlayerJoined -= AssignInputs;
+
+             //Now we can grab the inputs and assign them to this script
+             playerControls = InputManager.instance.players[playerID].playerControls;
+
+             playerControls.MasterActions.NorthButton.performed += NorthButton_performed;
+             playerControls.MasterActions.NorthButton.canceled += NorthButton_canceled;
+
+             playerControls.MasterActions.SouthButton.performed += SouthButton_performed;
+             playerControls.MasterActions.SouthButton.canceled += SouthButton_canceled;
+
+             playerControls.MasterActions.EastButton.performed += EastButton_performed;
+             playerControls.MasterActions.EastButton.canceled += EastButton_canceled;
+
+             playerControls.MasterActions.WestButton.performed += WestButton_performed;
+             playerControls.MasterActions.WestButton.canceled += WestButton_canceled;
+
+             playerControls.MasterActions.Movement.performed += Movement_performed;
+             playerControls.MasterActions.Movement.canceled += Movement_canceled;
+
+             waitingGrp.SetActive(false);
+             inputGrp.SetActive(true);
+         }
+     }
+
+
+     //Below are just functions for turning gameobjects on/off based on the button thats been pressed
+     private void NorthButton_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         northButton.SetActive(true);
+     }
+     private void NorthButton_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         northButton.SetActive(false);
+     }
+
+     private void SouthButton_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         southButton.SetActive(true);
+     }
+     private void SouthButton_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         southButton.SetActive(false);
+     }
+
+     private void EastButton_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         eastButton.SetActive(true);
+     }
+     private void EastButton_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         eastButton.SetActive(false);
+     }
+
+     private void WestButton_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         westButton.SetActive(true);
+     }
+     private void WestButton_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         westButton.SetActive(false);
+     }
+
+     //This is for the analog movement
+     private void Movement_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         analogInput = obj.ReadValue<Vector2>();
+         UpdateAnalogVisuals();
+     }
+     private void Movement_canceled(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+     {
+         analogInput = Vector2.zero;
+         UpdateAnalogVisuals();
+     }
+
+     void UpdateAnalogVisuals()
+     {
+         analogStick.localPosition = analogInput * 20; //135 is the multipler to make it go all the way to the edge
+     }
+    */
+}
