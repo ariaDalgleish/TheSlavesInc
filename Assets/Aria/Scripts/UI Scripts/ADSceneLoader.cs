6a065cb8734306c93db3369ac8cfaf9bbb448a73
ariaDalgleish <dalgleisharia07@gmail.com>
2024-08-20 13:51:26 +1200

    Scene Managing
    Aria-  changed KiKi scene

--- Assets/Aria/Scripts/UI Scripts/ADSceneLoader.cs
+++ Assets/Aria/Scripts/UI Scripts/ADSceneLoader.cs
@@ -5,7 +5,8 @@ public class ADSceneLoader : MonoBehaviour
 {
     private void Start()
     {
-        
+        SceneManager.LoadScene("Enemy", LoadSceneMode.Additive);
+        SceneManager.LoadScene("PuzzleScene", LoadSceneMode.Additive);
         SceneManager.LoadScene("UI", LoadSceneMode.Additive);
     }
 }
