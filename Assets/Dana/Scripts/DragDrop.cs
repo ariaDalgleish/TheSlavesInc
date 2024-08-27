c38011acaa8760dfdfa55ff03f5702a94df85897
hheuep <danaxdhh@gmail.com>
2024-08-27 15:58:03 +1200

    .

--- Assets/Dana/Scripts/DragDrop.cs
+++ Assets/Dana/Scripts/DragDrop.cs
@@ -5,22 +5,42 @@ using UnityEngine;
 public class DragDrop : MonoBehaviour
 {
     public GameObject objectToDrag;
-    public GameObject objectDragToPos;
-
-    public float Dropdistance;
+    public GameObject holder; // Corresponding Holder object for this drag object
+    public float Dropdistance = 50f;
     public bool isLocked;
 
-    Vector2 objectInitPos;
-
+    private Vector2 objectInitPos;
+    public GameObject puzzleClearPanel; // Reference to the puzzle clear panel
+    private bool puzzleCompleted = false; // Track if the puzzle has been completed
 
     void Start()
     {
         objectInitPos = objectToDrag.transform.position;
+
+        // Hide all initially
+        holder.SetActive(false);
+        puzzleClearPanel.SetActive(false);
+
+        // Randomly activate 4-5 holders
+        if (RandomHolderActivation())
+        {
+            holder.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        // Check for 'E' key press to hide the panel
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HidePuzzleClearScreen();
+            RandomHolderActivation();
+        }
     }
 
     public void DragObject()
     {
-        if(!isLocked)
+        if (!isLocked)
         {
             objectToDrag.transform.position = Input.mousePosition;
         }
@@ -28,17 +48,59 @@ public class DragDrop : MonoBehaviour
 
     public void DropObject()
     {
-        float Distance = Vector3.Distance(objectToDrag.transform.position, objectDragToPos.transform.position);
-        if(Distance < Dropdistance)
+        float Distance = Vector3.Distance(objectToDrag.transform.position, holder.transform.position);
+        if (Distance < Dropdistance)
         {
             isLocked = true;
-            objectToDrag.transform.position = objectDragToPos.transform.position;
+            objectToDrag.transform.position = holder.transform.position; // Snap to holder position
+
+            // Check if all objects are locked after this one is dropped correctly
+            CheckGameClear();
         }
         else
         {
-            objectToDrag.transform.position = objectInitPos;
+            objectToDrag.transform.position = objectInitPos; // Return to initial position if not dropped correctly
         }
     }
 
+    void CheckGameClear()
+    {
+        DragDrop[] allDragDrops = FindObjectsOfType<DragDrop>();
+        foreach (DragDrop dragDrop in allDragDrops)
+        {
+            if (!dragDrop.isLocked)
+            {
+                return; // Exit if any object is not locked, meaning the game is not yet cleared
+            }
+        }
+
+        puzzleCompleted = true;
+        ShowPuzzleClearScreen();
+        Debug.Log("Game Cleared!"); // This will only run when all objects are locked
+        // Trigger additional game clear actions here, if needed
+    }
+
+    // Method to randomly determine if a holder should be activated
+    bool RandomHolderActivation()
+    {
+        // Randomly activate between 4 and 5 holders
+        int randomValue = Random.Range(0, 8); // Random value between 0 and 7 (8 possibilities)
+        return randomValue < 5; // True for 4-5 times out of 8
+    }
+
+    private void ShowPuzzleClearScreen()
+    {
+        if (puzzleClearPanel != null)
+        {
+            puzzleClearPanel.SetActive(true);
+        }
+    }
+    public void HidePuzzleClearScreen()
+    {
+        if (puzzleClearPanel != null)
+        {
+            puzzleClearPanel.SetActive(false);
+        }
+    }
 
 }
