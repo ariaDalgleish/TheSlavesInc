907e9fc460a78b99eaebff584347be6deb264c61
hheuep <danaxdhh@gmail.com>
2024-09-17 04:44:13 +0900

    assets added

---
+++ Assets/Dana/puzzle/CoinMagnet.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CoinMagnet : MonoBehaviour
+{
+    public List<GameObject> coins = new List<GameObject>();
+    public float magnetForce;
+    public float speed;
+
+    void Start()
+    {
+        foreach (var coin in GameObject.FindGameObjectsWithTag("coin"))
+        {
+            coins.Add(coin);
+        }
+    }
+
+    void Update()
+    {
+        foreach(var coin in coins)
+        {
+            if(coin != null)
+            {
+                float distance = Vector2.Distance(transform.position, coin.transform.position);
+                if (distance < magnetForce)
+                {
+                    coin.transform.position = Vector2.Lerp(coin.transform.position, transform.position, speed);
+                }
+            }
+            
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "coin")
+        {
+            Destroy(collision.gameObject);
+            coins.Remove(collision.gameObject);
+        }
+    }
+}
